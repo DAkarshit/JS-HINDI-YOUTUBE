b8c1ff6e416f4d57fee08eef4db72a163e1f84c2
DAkarshit <154973732+DAkarshit@users.noreply.github.com>
2023-12-28 07:03:26 +0000

    datatypes and variables

---
+++ .devcontainer/01_basic/01_variables.js
@@ -0,0 +1,21 @@
+/*Constant and Variables*/
+const accountId = 144553 /*can't change this value*/
+let accountEmail ="akarshitdhiman890@gmail.com"
+var accountPassword = "12345"
+accountCity = "Himachal" 
+let  accountState // if you declare a variable and leave then you see the value is undefined//
+
+//accountId = 2// not allowed
+accountEmail ="akarshitdhiman890.o@gmail.com"
+accountPassword="54321"
+accountCity="Hp"
+
+console.log(accountId);
+
+console.table([accountEmail,accountId,accountPassword,accountCity,accountState])
+
+//Scope-- {} this is called  scope //
+/*That's why let is more efficience then var 
+Prefer not to use var 
+because of issue in block scope and functional scope
+*/
---
+++ .devcontainer/01_basic/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all JS code as newer version  //
+//alert(3+3)// we are using nodejs,not browser
+console.log(3+
+    3);// code redadbility should  be high//
+
+console.log("rupam")
+ 
+let name ="Akarshit"
+let age =18
+let isLoggedIn =false //boolean
+let state =null //this is null 
+let statee; // this is un defiened
+
+// number => 2 to power 53 
+// bigInt
+// string  => ""
+// boolean => true/false 
+// null => standalone value 
+// undefiend =>
+// symbol =>unique 
+
+// object 
+
+console.log(typeof "abu")
+console.log(typeof age)
+console.log(typeof null) //null=object
+console.log(typeof undefined)// undefined
