ed2440612bf88f716852c4c49428dbc7256d1a38
DAkarshit <154973732+DAkarshit@users.noreply.github.com>
2023-12-28 07:36:22 +0000

    conversion

---
+++ .devcontainer/01_basic/03_conversionOperation.js
@@ -0,0 +1,41 @@
+//let score = "33abc"//=>NaN
+//let score=null//
+//let score=undefined//
+//let score= true// true=>1;false=>0
+//let score="abu"// NaN
+/*console.log(typeof score)
+console.log(typeof(score))
+
+let scoree = "33"
+
+console.log(typeof scoree)
+console.log(typeof(scoree))
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log( valueInNumber)//NaN=not a number//
+
+let isLoggedIn = 1 //it gives true //
+
+let booleanisLoggedIn = Boolean(isLoggedIn)
+console.log(booleanisLoggedIn);
+
+let isLoggedInn = 0 //it gives false //
+
+let booleanisLoggedInn = Boolean(isLoggedIn)
+console.log(booleanisLoggedInn);
+
+let isLoggedInnn = "" //it gives false // 
+
+let booleanisLoggedInnn = Boolean(isLoggedIn)
+console.log(booleanisLoggedInnn);
+
+let isLoggedInnnn = "Akarshit" //it gives true // 
+
+let booleanisLoggedInnnn = Boolean(isLoggedIn)
+console.log(booleanisLoggedInnnn);*/
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
