59e44c0179ff2ef708e9675e32cb8a9615ff0a42
DAkarshit <154973732+DAkarshit@users.noreply.github.com>
2024-01-02 06:22:22 +0000

    datatype summary

---
+++ .devcontainer/01_basic/datatypes-summary.js
@@ -0,0 +1,36 @@
+// Primitive
+
+// 7 types : String,Number,Boolean,null,undefiened,Symbol,BigInt
+
+const score = 100
+const scoreValue =100.3
+
+const isLoggedIn = false
+const outsideTemp = null 
+let userEmail;
+
+const id =Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id=== anotherId)
+
+//const bigNumber = 576964679896467547646n
+
+// Reference (Non primitive)
+
+// Array,Objects,Functions
+
+const heros=["NAVY","ARMY","AIRFORCE"]
+let myobj=
+// object
+{
+    name: "Akarshit",
+    age: 23,
+}
+
+const myFunction=-function() 
+{
+console.log("hello world")
+}
+
+console.log(typeof outsideTemp)
